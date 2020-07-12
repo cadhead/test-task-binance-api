0a3ce984ae8f1ed31de3e43bc77becc7371b03a4
OutsiderVsTheWorld <nekorides@gmail.com>
2020-07-13 00:30:51 +0300

    add difflist

---
+++ src/components/diffList-component.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="content">
+    <div class="title">{{ current }}</div>
+    <div class="box">
+      <select v-model="current">
+        <option
+          v-for="(item, index) in items"
+          :key="index"
+          >{{ item }}</option
+        >
+      </select>
+      <ul>
+        <li>Что?</li>
+        <li>Тут?</li>
+        <li>Должно?</li>
+        <li>Быть?</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: ['BTCUSDT', 'BNBBTC', 'ETHBTC'],
+    current: 'BTCUSDT',
+  }),
+  watch: {
+    current(symbol) {
+      this.$bus.$emit('symbol', symbol);
+    },
+  },
+};
+</script>
