c5019a90b5dae6a37796203e668b3c654ffc28e6
OutsiderVsTheWorld <nekorides@gmail.com>
2020-07-12 22:48:53 +0300

    add buefy for styling

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-import Core from './plugins/app-core';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 
+import Core from './plugins/app-core';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 Vue.use(Core);
+Vue.use(Buefy);
 
 axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://www.binance.com/api/v3/';
 
