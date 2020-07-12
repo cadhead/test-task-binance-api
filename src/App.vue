4a1f0f04607d0ad7bf042ceaf699911327bbf6c5
OutsiderVsTheWorld <nekorides@gmail.com>
2020-07-12 23:19:22 +0300

    add navtabs to App

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,34 @@
     <h1 class="title">SDK для взимодействия с биржей binance</h1>
     <p class="subtitle">Ориентировочное время выполнения 4-8 часов</p>
     <div class="pt-6">
-      <p>tabs here</p>
+      <navTabs :data="navTabsList" />
       <p>content here</p>
     </div>
   </div>
 </template>
 
 <script>
+import navTabs from '@/components/navtabs-component.vue';
 
 export default {
   name: 'App',
   components: {
+    navTabs,
+  },
+  data() {
+    return {
+      navTabsList: {
+        items: [
+          { name: 'GlassState', niceName: 'Стаканы' },
+        ],
+        current: 'GlassState',
+      },
+    };
+  },
+  computed: {
+    currentTabComponent() {
+      return this.navTabs.current;
+    },
   },
 };
 </script>
