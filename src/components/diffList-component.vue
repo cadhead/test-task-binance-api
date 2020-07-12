f430b696a565e29987e62272785d050b3415eb3a
OutsiderVsTheWorld <nekorides@gmail.com>
2020-07-13 01:06:53 +0300

    whatever

--- src/components/diffList-component.vue
+++ src/components/diffList-component.vue
@@ -2,6 +2,7 @@
   <div class="content">
     <div class="title">{{ current }}</div>
     <div class="box">
+      <p class="subtitle">Выберите символ: </p>
       <select v-model="current">
         <option
           v-for="(item, index) in items"
@@ -10,10 +11,13 @@
         >
       </select>
       <ul>
-        <li>Что?</li>
-        <li>Тут?</li>
-        <li>Должно?</li>
-        <li>Быть?</li>
+        <li
+          v-for="(change, index) in changes"
+          :key="index"
+        >
+          [{{ change.date }}] Символ изменён на
+          <span class="tag is-info">{{ change.symbol }}</span>
+        </li>
       </ul>
     </div>
   </div>
@@ -24,10 +28,24 @@ export default {
   data: () => ({
     items: ['BTCUSDT', 'BNBBTC', 'ETHBTC'],
     current: 'BTCUSDT',
+    changes: [],
   }),
   watch: {
     current(symbol) {
       this.$bus.$emit('symbol', symbol);
+
+      this.changes.push({
+        symbol,
+        date: new Date().toLocaleString('ru', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          timezone: 'UTC',
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+        }),
+      });
     },
   },
 };
