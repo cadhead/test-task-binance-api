87bf78caeb1244c95c8943c387bf65735b2d1a52
OutsiderVsTheWorld <nekorides@gmail.com>
2020-07-12 23:10:56 +0300

    add navtabs gui

---
+++ src/components/navtabs-component.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tabs">
+    <ul>
+      <li
+        v-for="(tab, index) in data.items"
+        :key="index"
+        :class="[{ 'is-active': data.current === tab.name }]"
+        @click="switchTab(tab)"
+      ><a>{{ tab.niceName }}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  methods: {
+    switchTab(tab) {
+      this.$emit('switchTab', tab);
+    },
+  },
+};
+</script>
